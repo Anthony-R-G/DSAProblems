3a71a2cebec67325925c54c36b5e1013b27bf2c5
Anthony <52180531+Anthony-R-G@users.noreply.github.com>
2020-07-18 18:47:33 -0400

    Create BestTimeToBuyAndSellStock.swift

---
+++ LeetCode/BestTimeToBuyAndSellStock.swift
@@ -0,0 +1,34 @@
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Note that you cannot sell a stock before you buy one.
+
+input = [7,1,5,3,6,4]
+Output = 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+             Not 7-1 = 6, as selling price needs to be larger than buying price.
+
+input = [7,6,4,3,1]
+Output = 0
+
+input = [2,4,1]
+Output = 2
+*/
+
+func maxProfit(_ prices: [Int]) -> Int {
+  var minPrice = Int.max
+  var maxProfit = Int()
+
+  for stockPrice in prices {
+    if stockPrice < minPrice {
+      minPrice = stockPrice
+    }
+
+    if (stockPrice - minPrice) > maxProfit {
+      maxProfit = stockPrice - minPrice
+    }
+  }
+  return maxProfit
+}
